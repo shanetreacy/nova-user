a81fd702bf832bf6626a04f7c1c96daaaf48e6fd
Shane Treacy <shane.treacy@emc.com>
2018-05-19 15:42:22 +0100

    first commit

---
+++ src/main/java/io/treacy/novauser/NovaUserApplication.java
@@ -0,0 +1,12 @@
+package io.treacy.novauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaUserApplication.class, args);
+	}
+}
